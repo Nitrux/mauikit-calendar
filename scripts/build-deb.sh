c261c85f35b75abd81e3a353b7e5de1b88eafe2d
Uri Herrera <hello@uriherrera.com>
2022-11-28 23:19:06 -0600

    create package

---
+++ scripts/build-deb.sh
@@ -0,0 +1,83 @@
+#! /bin/bash
+
+set -x
+
+### Update sources
+
+wget -qO /etc/apt/sources.list.d/nitrux-main-compat-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources/nitrux-repo.list
+
+wget -qO /etc/apt/sources.list.d/nitrux-testing-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources/nitrux-testing-repo.list
+
+curl -L https://packagecloud.io/nitrux/repo/gpgkey | apt-key add -;
+curl -L https://packagecloud.io/nitrux/compat/gpgkey | apt-key add -;
+curl -L https://packagecloud.io/nitrux/testing/gpgkey | apt-key add -;
+
+apt -qq update
+
+### Install Package Build Dependencies #2
+
+apt -qq -yy install --no-install-recommends \
+	mauikit-git
+
+### Download Source
+
+git clone --depth 1 --branch $MAUIKIT_ACCOUNTS_BRANCH https://invent.kde.org/maui/mauikit-calendar.git
+
+rm -rf mauikit-calendar/{LICENSE,README.md,licenses}
+
+### Compile Source
+
+mkdir -p build && cd build
+
+cmake \
+	-DCMAKE_INSTALL_PREFIX=/usr \
+	-DENABLE_BSYMBOLICFUNCTIONS=OFF \
+	-DQUICK_COMPILER=ON \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_SYSCONFDIR=/etc \
+	-DCMAKE_INSTALL_LOCALSTATEDIR=/var \
+	-DCMAKE_EXPORT_NO_PACKAGE_REGISTRY=ON \
+	-DCMAKE_FIND_PACKAGE_NO_PACKAGE_REGISTRY=ON \
+	-DCMAKE_INSTALL_RUNSTATEDIR=/run "-GUnix Makefiles" \
+	-DCMAKE_VERBOSE_MAKEFILE=ON \
+	-DCMAKE_INSTALL_LIBDIR=lib/x86_64-linux-gnu ../mauikit-calendar/
+
+make -j$(nproc)
+
+make install
+
+### Run checkinstall and Build Debian Package
+
+>> description-pak printf "%s\n" \
+	'A free and modular front-end framework for developing user experiences.' \
+	'' \
+	'Calendar support components for Maui applications.' \
+	'' \
+	'Maui stands for Multi-Adaptable User Interface and allows ' \
+	'any Maui app to run on various platforms + devices,' \
+	'like Linux Desktop and Phones, Android, or Windows.' \
+	'' \
+	'This package contains the MauiKit accounts shared library, the MauiKit accounts qml module' \
+	'and the MauiKit accounts development files.' \
+	'' \
+	''
+
+checkinstall -D -y \
+	--install=no \
+	--fstrans=yes \
+	--pkgname=mauikit-calendar-git \
+	--pkgversion=$PACKAGE_VERSION \
+	--pkgarch=amd64 \
+	--pkgrelease="1" \
+	--pkglicense=LGPL-3 \
+	--pkggroup=libs \
+	--pkgsource=mauikit-calendar \
+	--pakdir=. \
+	--maintainer=uri_herrera@nxos.org \
+	--provides=mauikit-calendar-git \
+	--requires="libc6,mauikit-git \(\>= 2.2.1+git\),libkf5akonadicore5abi2,libkf5akonadiprivate5abi2,libkf5akonadiwidgets5abi1, libkf5akonadixml5,libkf5akonadiagentbase5,libqt5core5a,libqt5qml5,libqt5sql5,libstdc++6,qml-module-org-kde-kirigami2,qml-module-qtquick-controls2,qml-module-qtquick-shapes" \
+	--nodoc \
+	--strip=no \
+	--stripso=yes \
+	--reset-uids=yes \
+	--deldesc=yes
