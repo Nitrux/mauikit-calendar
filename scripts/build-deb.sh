59e595abab7e9a14a7909005c515f50ddb52bb0a
Uri Herrera <hello@uriherrera.com>
2025-06-13 15:16:50 -0600

    update ci scripts

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -1,33 +1,45 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
-set -eu
+#############################################################################################################################################################################
+#   The license used for this file and its contents is: BSD-3-Clause                                                                                                        #
+#                                                                                                                                                                           #
+#   Copyright <2025> <Uri Herrera <uri_herrera@nxos.org>>                                                                                                                   #
+#                                                                                                                                                                           #
+#   Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:                          #
+#                                                                                                                                                                           #
+#    1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.                                        #
+#                                                                                                                                                                           #
+#    2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer                                      #
+#       in the documentation and/or other materials provided with the distribution.                                                                                         #
+#                                                                                                                                                                           #
+#    3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software                    #
+#       without specific prior written permission.                                                                                                                          #
+#                                                                                                                                                                           #
+#    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,                      #
+#    THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS                  #
+#    BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE                 #
+#    GOODS OR SERVICES; LOSS OF USE, DATA,   OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,                      #
+#    STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   #
+#############################################################################################################################################################################
 
-### Update sources
 
-mkdir -p /etc/apt/keyrings
+# -- Exit on errors.
 
-curl -fsSL https://packagecloud.io/nitrux/mauikit/gpgkey | gpg --dearmor -o /etc/apt/keyrings/nitrux_mauikit-archive-keyring.gpg
+set -e
 
-cat <<EOF > /etc/apt/sources.list.d/nitrux-mauikit.list
-deb [signed-by=/etc/apt/keyrings/nitrux_mauikit-archive-keyring.gpg] https://packagecloud.io/nitrux/mauikit/debian/ trixie main
-EOF
 
-apt -q update
+# -- Download Source
 
-### Install Package Build Dependencies #2
-
-apt -qq -yy install --no-install-recommends \
-	mauikit-git
-
-### Download Source
-
-git clone --depth 1 --branch $MAUIKIT_CALENDAR_BRANCH https://invent.kde.org/maui/mauikit-calendar.git
+git clone --depth 1 --branch "$MAUIKIT_CALENDAR_BRANCH" https://invent.kde.org/maui/mauikit-calendar.git
 
 rm -rf mauikit-calendar/{LICENSE,README.md,licenses}
 
-### Compile Source
+
+# -- Compile Source
 
 mkdir -p build && cd build
+
+HOST_MULTIARCH=$(dpkg-architecture -qDEB_HOST_MULTIARCH)
 
 cmake \
 	-DCMAKE_INSTALL_PREFIX=/usr \
@@ -40,7 +52,8 @@ cmake \
 	-DCMAKE_FIND_PACKAGE_NO_PACKAGE_REGISTRY=ON \
 	-DCMAKE_INSTALL_RUNSTATEDIR=/run "-GUnix Makefiles" \
 	-DCMAKE_VERBOSE_MAKEFILE=ON \
-	-DCMAKE_INSTALL_LIBDIR=/usr/lib/x86_64-linux-gnu ../mauikit-calendar/
+	-DCMAKE_INSTALL_LIBDIR="/usr/lib/${HOST_MULTIARCH}" \
+	../mauikit-calendar/
 
 make -j"$(nproc)"
 
@@ -65,17 +78,17 @@ make install
 checkinstall -D -y \
 	--install=no \
 	--fstrans=yes \
-	--pkgname=mauikit-calendar-git \
-	--pkgversion=$PACKAGE_VERSION \
-	--pkgarch=amd64 \
+	--pkgname=mauikit-calendar \
+	--pkgversion="$PACKAGE_VERSION" \
+	--pkgarch="$(dpkg --print-architecture)" \
 	--pkgrelease="1" \
 	--pkglicense=LGPL-3 \
 	--pkggroup=libs \
 	--pkgsource=mauikit-calendar \
 	--pakdir=. \
 	--maintainer=uri_herrera@nxos.org \
-	--provides=mauikit-calendar-git \
-	--requires="libc6,libkf6calendarcore6,libkf6configcore6,libkf6contacts6,libkf6coreaddons6,libkf6i18n6,libkf6kiocore6,libkf6widgetsaddons6,libkpim6akonadiagentbase6,libkpim6akonadicontactcore6,libkpim6akonadicontactwidgets6,libkpim6akonadicore6,libkpim6akonadimime6,libkpim6akonadiprivate6,libkpim6akonadiwidgets6,libkpim6akonadixml6,libkpim6calendarutils6,libkpim6identitymanagementcore6,libkpim6identitymanagementquick6,libkpim6identitymanagementwidgets6,libqt6core5compat6,libqt6core6t64,libqt6qml6,libqt6sql6,libqt6svg6,mauikit-git \(\>= 4.0.1\),qml6-module-org-kde-calendarcore,qml6-module-org-kde-contacts,qml6-module-org-kde-kidentitymanagement,qml6-module-org-kde-kirigami,qml6-module-qtquick-controls,qml6-module-qtquick-shapes" \
+	--provides=mauikit-calendar \
+	--requires="libc6,libkf6calendarcore6,libkf6configcore6,libkf6contacts6,libkf6coreaddons6,libkf6i18n6,libkf6kiocore6,libkf6widgetsaddons6,libkpim6akonadiagentbase6,libkpim6akonadicontactcore6,libkpim6akonadicontactwidgets6,libkpim6akonadicore6,libkpim6akonadimime6,libkpim6akonadiprivate6,libkpim6akonadiwidgets6,libkpim6akonadixml6,libkpim6calendarutils6,libkpim6identitymanagementcore6,libkpim6identitymanagementquick6,libkpim6identitymanagementwidgets6,libqt6core5compat6,libqt6core6t64,libqt6qml6,libqt6sql6,libqt6svg6,mauikit-t \(\>= 4.0.2\),qml6-module-org-kde-calendarcore,qml6-module-org-kde-contacts,qml6-module-org-kde-kidentitymanagement,qml6-module-org-kde-kirigami,qml6-module-qtquick-controls,qml6-module-qtquick-shapes" \
 	--nodoc \
 	--strip=no \
 	--stripso=yes \
